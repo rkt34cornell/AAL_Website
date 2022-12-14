59c0046e48235d20f9ea8a5707fa2da90f520d76
djrobin17 <devangjain239@gmail.com>
2022-12-14 05:13:20 -0500

    fixed maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import './map-style.css'
 import styles from "../style";
+import WorldMap from 'react-world-map'
+import "./map-style.css"
 
 const Map = () => {
   return (
-    <div id="map" className={`${styles.marginY}`}>
+    <div id="map" className={`${styles.marginY} flex flex-col justify-center items-center`}>
     <h2 className={styles.heading2}>
         Map
       </h2>
-    <div className="w-[85%] sm:ml-10">
-      <ComposableMap> 
-        <Geographies geography="/features.json">
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography key={geo.rsmKey} geography={geo} />
-            ))
-          }
-        </Geographies>
-      </ComposableMap>
-    </div>
+      <div className="">
+      <WorldMap />
+      </div>
     </div>
   );
 };
