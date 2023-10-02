f602d6583b3bf9941298c29b9d4be60c441a3502
rkt34cornell <146210016+rkt34cornell@users.noreply.github.com>
2023-10-01 20:20:45 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -90,52 +90,51 @@ const Hero = () => {
         </div>
 
 <Particles
-          id="satellite1"
-          init={oldParticlesInit}
-          options={{
-            interactivity: {
-              events: {
-                onClick: {
-                  enable: true,
-                  mode: "attract",
-                },
-                onHover: {
-                  enable: true,
-                  mode: "grab",
-                },
-              },
-            },
-            fullScreen: {
-              enable: true,
-              zIndex: 0,
-            },
-            particles: {
-              shape: {
-                type: "image",
-                image: {
-                  src: sat1,
-                  replace_color: "#00ff00",
-                },
-              },
-              move: {
-                enable: true,
-                speed: 0.5,
-              },
-              size: {
-                value: 10,
-              },
-              number: {
-                value: 1,
-              },
-              color: {
-                value: "#00ff00", // Satellite color (you can change it)
-              },
-              line_linked: {
-                color: "#00ff00", // Line color (green)
-              },
-            },
-          }}
-        />
+  id="satellite1"
+  init={oldParticlesInit}
+  options={{
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "attract",
+        },
+        onHover: {
+          enable: true,
+          mode: "grab",
+        },
+      },
+    },
+    fullScreen: {
+      enable: true,
+      zIndex: 0,
+    },
+    particles: {
+      shape: {
+        type: "image",
+        image: {
+          src: sat1,
+        },
+      },
+      move: {
+        enable: true,
+        speed: 0.5,
+      },
+      size: {
+        value: 10,
+      },
+      number: {
+        value: 1,
+      },
+      color: {
+        value: "#ffffff", // Satellite color (white)
+      },
+      line_linked: {
+        color: "#00ff00", // Line color (green)
+      },
+    },
+  }}
+/>
 
         <Particles
           id="satellite2"
