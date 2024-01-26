eaaec06de546864e3355fc0dad3e5b9310d42859
Rajiv <rkt34@cornell.edu>
2024-01-25 23:18:51 -0500

    testing team

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ProjectsPage from "./pages/Projects";
+import Publications from "./pages/Publications";
+import Team from "./pages/Team";
+import Map from "./pages/Map";
+import Taxonomy from "./pages/Taxonomy";
+
 // ... other imports
 
 const aalaudio = "https://res.cloudinary.com/dlxh3nrry/video/upload/v1706240962/AALMUSIC_fiyfdy.mp3";
@@ -24,6 +29,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/projects" element={<ProjectsPage />} />
+        <Route exact path="/team" element={<Team/>} />
+
         // ... other routes
       </Routes>
       {/* Place any global components like Footer here */}
