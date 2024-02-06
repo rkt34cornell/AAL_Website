b7bb121f74a8bcaa957e9e5a7d7d3445526f3873
Rajiv <rkt34@cornell.edu>
2024-02-05 21:47:52 -0500

    bye bye impact

--- src/components/Impact.jsx
+++ src/components/Impact.jsx
@@ -1,31 +1,32 @@
-import { impact } from "../constants";
-import styles from "../style";
+const Impact = () => {
+  if (impact.length === 0) return null; // or return <></> for an empty fragment
 
-const Impact = () => (
-  <div id="impact">
-    <div
-      className={`flex-1 ${styles.flexCenter} flex-col xl:px-0 sm:px-16 px-6 mb-10`}
-    >
-      <h2 className={styles.heading2}>Impact</h2>
+  return (
+    <div id="impact">
+      <div
+        className={`flex-1 ${styles.flexCenter} flex-col xl:px-0 sm:px-16 px-6 mb-10`}
+      >
+        <h2 className={styles.heading2}>Impact</h2>
+      </div>
+      <section
+        className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
+      >
+        {impact.map((impact) => (
+          <div
+            key={impact.id}
+            className={`flex-1 flex justify-center items-center flex-row m-3`}
+          >
+            <h4 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[53.16px] leading-[43.16px] text-secondary">
+              {impact.value}
+            </h4>
+            <p className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-white uppercase ml-3">
+              {impact.title}
+            </p>
+          </div>
+        ))}
+      </section>
     </div>
-    <section
-      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
-    >
-      {impact.map((impact) => (
-        <div
-          key={impact.id}
-          className={`flex-1 flex justify-center items-center flex-row m-3`}
-        >
-          <h4 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[53.16px] leading-[43.16px] text-secondary">
-            {impact.value}
-          </h4>
-          <p className="font-poppins font-normal xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-white uppercase ml-3">
-            {impact.title}
-          </p>
-        </div>
-      ))}
-    </section>
-  </div>
-);
+  );
+};
 
 export default Impact;
