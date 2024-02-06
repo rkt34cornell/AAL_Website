0aba0d256837d87b0e14f0d038471b174d85e6a8
Rajiv <rkt34@cornell.edu>
2024-02-05 22:11:12 -0500

    testing impact revert

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -19,7 +19,8 @@ const Homepage = () => {
     
     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        //<Impact />
+        {/* <Impact /> */}
+
         <Projects />
         <Partners />
         {/* <Team /> */}
