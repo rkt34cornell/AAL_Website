c2eb2f4adba4e85ad1f781f545d88be27a591d62
Rajiv <rkt34@cornell.edu>
2024-02-06 22:48:44 -0500

    tesitng 2

--- src/pages/Present2024.jsx
+++ src/pages/Present2024.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Navbar, Footer, ProjectsPanel } from "../components"; // Make sure to import ProjectsPanel
-import projectsData from '../constants/projectsData.json'; // Import your projects data
+import { Navbar, Footer, ProjectsPanel } from "../components";
+import projectsData from '../constants/projectsData.json';
 import styles from '../style';
 
 const Present2024 = () => {
-  // You might want to filter projectsData to only include projects relevant to 2024 - Present
   const present2024Projects = projectsData.filter(project => project.period === '2024-present');
 
   return (
@@ -12,13 +11,12 @@ const Present2024 = () => {
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
-          <h2 className={`${styles.heading2}`}>
-            2024 - Present Projects
+          <h2 className={`${styles.heading2} text-center`}> {/* Ensure the heading is centered */}
+            Research
           </h2>
-          <p className={`${styles.paragraph} mt-5 mb-5 text-justify`}>
-            Content for 2024 - Present Projects goes here. Highlight ongoing projects, future plans, and visions for this period.
+          <p className={`${styles.paragraph} mt-5 mb-5 text-center italic`}> {/* Add text-center and italic for Tailwind CSS */}
+            Driving the next generation of national security space capabilities.
           </p>
-          {/* Integrate ProjectsPanel to display projects for 2024 - Present */}
           <ProjectsPanel projects={present2024Projects} />
           <Footer />
         </div>
