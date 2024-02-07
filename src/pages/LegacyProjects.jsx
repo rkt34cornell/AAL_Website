de717ef2a66e3ec76feb7555866d690e7f8b6695
Rajiv <rkt34@cornell.edu>
2024-02-06 21:23:17 -0500

    adding content to 2 new pages

--- src/pages/LegacyProjects.jsx
+++ src/pages/LegacyProjects.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Navbar, Footer } from "../components";
+import styles from '../style';
 
-function LegacyProjects() {
+const LegacyProjects = () => {
   return (
-    <div>
-      <h1>Legacy Projects</h1>
-      {/* Content for the Legacy Projects page goes here */}
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+          <h2 className={`${styles.heading2}`}>
+            Legacy Projects
+          </h2>
+          <p className={`${styles.paragraph} mt-5 mb-5 text-justify`}>
+            Content for Legacy Projects goes here. Describe the projects, goals, achievements, and any other relevant information.
+          </p>
+          {/* Content and structure specific to Legacy Projects can be added here */}
+          <Footer />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default LegacyProjects;
--- src/pages/Present2024.jsx
+++ src/pages/Present2024.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Navbar, Footer } from "../components";
+import styles from '../style';
 
-function Present2024() {
+const Present2024 = () => {
   return (
-    <div>
-      <h1>2024 - Present</h1>
-      {/* Content for the 2024 - Present page goes here */}
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+          <h2 className={`${styles.heading2}`}>
+            2024 - Present Projects
+          </h2>
+          <p className={`${styles.paragraph} mt-5 mb-5 text-justify`}>
+            Content for 2024 - Present Projects goes here. Highlight ongoing projects, future plans, and visions for this period.
+          </p>
+          {/* Content and structure specific to 2024 - Present Projects can be added here */}
+          <Footer />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Present2024;
