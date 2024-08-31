145c4e0f86a050ef08a0c8f26540c687e6ae6c6a
rkt34cornell <146210016+rkt34cornell@users.noreply.github.com>
2024-08-31 00:32:05 -0400

    Update Team.jsx

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -54,109 +54,112 @@ const Team = () => {
           <div
             className={`${styles.paddingX} ${styles.paddingY} ${styles.flexCenter}`}
           >
-            <Card className="sm:w-96" shadow={false} color="white">
-              <CardHeader floated={false}>
-                <img src={greg} alt="greg" className="h-[100%] w-[100%]" />
-              </CardHeader>
-              <CardBody className="flex flex-row items-center justify-between">
-                <div>
-                  <Typography variant="h4" color="black" className="mb-2">
-                    Gregory Falco
-                  </Typography>
-                  <Typography
-                    color="black"
-                    className="font-medium flex justify-items-start gap-5"
-                  >
-                    PI
-                    <Tooltip content="LinkedIn">
-                      <Typography
-                        as="a"
-                        href="https://www.linkedin.com/in/gregoryfalco/"
-                        target="_blank"
-                      >
-                        <FontAwesomeIcon icon={faLinkedin} />
-                      </Typography>
-                    </Tooltip>
-                    <Tooltip content="ResearchGate">
-                      <Typography
-                        as="a"
-                        href="https://www.researchgate.net/profile/Gregory-Falco"
-                        target="_blank"
-                      >
-                        <FontAwesomeIcon icon={faResearchgate} />
-                      </Typography>
-                    </Tooltip>
-                    <Tooltip content="Twitter">
-                      <Typography
-                        as="a"
-                        href="https://twitter.com/gregfalco"
-                        target="_blank"
-                      >
-                        <FontAwesomeIcon icon={faTwitter} />
-                      </Typography>
-                    </Tooltip>
-                    <Tooltip content="Email">
-                      <Typography
-                        as="a"
-                        href="mailto: gfalco@cornell.edu"
-                        target="_blank"
-                      >
-                        <FontAwesomeIcon icon={faEnvelope} />
-                      </Typography>
-                    </Tooltip>
-                  </Typography>
-                </div>
-                <div>
-                  <Typography
-                    onClick={() => {
-                      setIndex(0);
-                    }}
-                    className="cursor-pointer"
-                  >
-                    {selectedIndex == 0 ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M4.5 15.75l7.5-7.5 7.5 7.5"
-                        />
-                      </svg>
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                        />
-                      </svg>
-                    )}
-                  </Typography>
-                </div>
-              </CardBody>
-              <div className={selectedIndex == 0 ? "block" : "hidden"}>
-                <CardFooter className="pt-0">
-                  <Typography color="black" className="text-start">
-                  Prof. Gregory Falco has been at the forefront of space system and critical infrastructure security and autonomy in both industry and academia for the past decade. Falco is an Assistant Professor at Cornell University’s Sibley School of Mechanical and Aerospace Engineering and the Systems Engineering Program. He is the Director of the Aerospace ADVERSARY (Autonomy, Defense and Vulnerability Exploitation for Resilient, Secure and Assured Risk/Yield) Lab. The ADVERSARY designs and develops autonomous, resilient and secure space infrastructure. He is leading the effort to develop an international technical standard for space cybersecurity as the chair of the IEEE Standard for Space System Cybersecurity working group. His research entitled Cybersecurity Principles for Space Systems was highly influential in the development of Space Policy Directive-5, which shared the same title. Falco was the former co-founder and CEO of the blockchain-based industrial control security company NeuroMesh Inc., which was acquired in 2022. He has been listed in Forbes 30 Under 30 for his inventions and contributions to critical infrastructure cyber security, is a Fulbright Scholar and is the recipient of the DARPA RISER and DARPA’s Young Faculty Award for work on building a zero-trust marketplace ecosystem for space systems. Prof. Falco serves as a member of the Department of Homeland Security’s Space Systems Critical Infrastructure Working Group and has been awarded contracts relating to space system security for AFRL, US Space Force, NASA and DARPA. He holds an appointment as a Research Affiliate at MIT’s Computer Science and Artificial Intelligence Laboratory and the Johns Hopkins University Applied Physics Laboratory’s Space Sector. Falco completed his PhD at MIT’s Computer Science and Artificial Intelligence Laboratory, Master’s degree at Columbia University and Bachelor’s degree at Cornell University.
+     <Card className="sm:w-96" shadow={false} color="white">
+  <CardHeader floated={false}>
+    <img src={greg} alt="greg" className="h-[100%] w-[100%]" />
+  </CardHeader>
+  <CardBody className="flex flex-row items-center justify-between">
+    <div>
+      <Typography variant="h4" color="black" className="mb-2">
+        Gregory Falco
+      </Typography>
+      <Typography
+        color="black"
+        className="font-medium flex justify-items-start gap-5"
+      >
+        PI
+        <Tooltip content="LinkedIn">
+          <Typography
+            as="a"
+            href="https://www.linkedin.com/in/gregoryfalco/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </Typography>
+        </Tooltip>
+        <Tooltip content="ResearchGate">
+          <Typography
+            as="a"
+            href="https://www.researchgate.net/profile/Gregory-Falco"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faResearchgate} />
+          </Typography>
+        </Tooltip>
+        <Tooltip content="Twitter">
+          <Typography
+            as="a"
+            href="https://twitter.com/gregfalco"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </Typography>
+        </Tooltip>
+        <Tooltip content="Email">
+          <Typography
+            as="a"
+            href="mailto: gfalco@cornell.edu"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
+          </Typography>
+        </Tooltip>
+      </Typography>
+    </div>
+    <div>
+      <Typography
+        onClick={() => {
+          setIndex(0);
+        }}
+        className="cursor-pointer"
+      >
+        {selectedIndex == 0 ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.5 15.75l7.5-7.5 7.5 7.5"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        )}
+      </Typography>
+    </div>
+  </CardBody>
+  {/* Horizontal dropdown for Dr. Gregory Falco */}
+  <div className={`${
+      selectedIndex == 0 ? "flex" : "hidden"
+    } flex-row items-start`}>
+    <CardFooter className="pt-0">
+      <Typography color="black" className="text-start">
+        {/* The content you want to display */}
+        Prof. Gregory Falco has been at the forefront of space system and critical infrastructure security...
+      </Typography>
+    </CardFooter>
+  </div>
+</Card>
 
-
-                  </Typography>
-                </CardFooter>
-              </div>
-            </Card>
           </div>
           <div className="grid gap-16 grid-cols-1 sm:grid-cols-3 sm:px-16 px-6 sm:py-16 py-6 justify-center items-start justify-items-center">
             <Card className="sm:w-96" shadow={false} color="white">
