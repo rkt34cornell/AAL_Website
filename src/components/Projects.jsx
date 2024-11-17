95d9a40fcdb7d6378d91cf154dc74f45772e1d53
SpaceInfrastructureLaboratory <spaceinfrastructurelab@gmail.com>
2024-11-16 19:22:29 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,9 +7,6 @@ const ProjectCard = ({ title, content, index }) => (
       index !== projects.length - 1 ? "mb-6" : "mb-0"
     } project-card`}
   >
-    {/* <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
-    </div> */}
     <div className="flex-1 flex flex-col">
       <h4 className="font-poppins font-semibold text-white text-[18px] mb-1">
         {title}
@@ -23,46 +20,13 @@ const ProjectCard = ({ title, content, index }) => (
 
 const Projects = () => (
   <section id="projects" className={`${layout.section} items-start justify-start`}>
-    {/* Remove the Research section */}
-    {/* <div className={`${layout.sectionInfo}`}>
-      <h2 className={styles.heading2}>Research</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        WE BREAK AEROSPACE SYSTEMS <br></br>
-        If you don’t know how to break it, you don’t know how to fix it: this is
-        the core of our many projects! <br></br>
-      </p>
-      <a href="/projects">
-        <button
-          type="button"
-          className="py-4 px-6 font-poppins font-medium text-[18px] text-black bg-white hover:bg-secondary rounded-[10px] outline-none mt-10 mb-10 md:mb-0"
-        >
-          Know More
-        </button>
-      </a>
-    </div> */}
-    
-    {/* Remove the Publications section */}
-    {/* <div className={`${layout.sectionInfo}`}>
-      <h2 className={styles.heading2}>
-        Publications
-      </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-      </p>
-      <a href="/publications">
-        <button type="button" className="py-4 px-6 font-poppins font-medium text-[18px] text-black bg-white hover:bg-secondary rounded-[10px] outline-none mt-10">
-          Know More
-        </button>
-      </a>
-    </div> */}
-    
-    {/* Uncomment this section if you want to display project cards */}
-    {/* <div
+    <div
       className={`${layout.sectionImg} flex-col md:scrollbar-thin md:scrollbar-thumb-secondary md:scrollbar-track-white md:h-96 md:overflow-y-scroll`}
     >
       {projects.map((projects, index) => (
         <ProjectCard key={projects.id} {...projects} index={index} />
       ))}
-    </div> */}
+    </div>
   </section>
 );
 
