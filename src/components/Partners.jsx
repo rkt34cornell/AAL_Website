0bf60187de2fd140d977bddc9c906d9a2059b4c3
rkt34cornell <rkt34@cornell.edu>
2025-12-25 21:07:06 -0500

    newglow

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from "../style";
 
 const afrl = "https://res.cloudinary.com/djrobin17/image/upload/q_auto:low,f_auto/v1684903703/the-aerospace-adversary/afrl_dzfaj2.png"
@@ -9,17 +9,40 @@ const darpa = "https://res.cloudinary.com/djrobin17/image/upload/q_auto:low,f_au
 const ussp = "https://res.cloudinary.com/djrobin17/image/upload/q_auto:low,f_auto/v1684903703/the-aerospace-adversary/ussp_vz5otz.png"
 
 const Partners = () => {
+  const sectionRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const node = sectionRef.current;
+    if (!node) {
+      return;
+    }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            observer.disconnect();
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+    observer.observe(node);
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <div>
+    <div ref={sectionRef}>
        <h2 className={`${styles.heading2} mt-8 mb-4`}>Our Parterners</h2>
     <div id="partners" className={`${styles.flexCenter} sm:flex-row flex-col`}>
       <div className='flex flex-row justify-center items-center'>
-      <img src={afrl} alt="afrl" className="partner-logo p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative" style={{ animationDelay: "0.05s" }} />
-      <img src={apl} alt="apl" className="partner-logo p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative" style={{ animationDelay: "0.15s" }} />
-      <img src={isac} alt="isac" className="partner-logo p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative" style={{ animationDelay: "0.25s" }} />
-      <img src={nist} alt="nist" className="partner-logo p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative" style={{ animationDelay: "0.35s" }} />
-      <img src={darpa} alt="darpa" className="partner-logo p-5 w-[20%] h-[20%] lg:p-10 lg:w-[20%] lg:h-[20%] z-1 relative" style={{ animationDelay: "0.45s" }} />
-      <img src={ussp} alt="ussp" className="partner-logo p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative" style={{ animationDelay: "0.55s" }} />
+      <img src={afrl} alt="afrl" className={`partner-logo ${isVisible ? "partner-logo--visible" : ""} p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative`} style={{ animationDelay: "0.05s" }} />
+      <img src={apl} alt="apl" className={`partner-logo ${isVisible ? "partner-logo--visible" : ""} p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative`} style={{ animationDelay: "0.15s" }} />
+      <img src={isac} alt="isac" className={`partner-logo ${isVisible ? "partner-logo--visible" : ""} p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative`} style={{ animationDelay: "0.25s" }} />
+      <img src={nist} alt="nist" className={`partner-logo ${isVisible ? "partner-logo--visible" : ""} p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative`} style={{ animationDelay: "0.35s" }} />
+      <img src={darpa} alt="darpa" className={`partner-logo ${isVisible ? "partner-logo--visible" : ""} p-5 w-[20%] h-[20%] lg:p-10 lg:w-[20%] lg:h-[20%] z-1 relative`} style={{ animationDelay: "0.45s" }} />
+      <img src={ussp} alt="ussp" className={`partner-logo ${isVisible ? "partner-logo--visible" : ""} p-5 w-[20%] h-[20%] lg:p-10 lg:w-[15%] lg:h-[15%] z-1 relative`} style={{ animationDelay: "0.55s" }} />
       </div>
     </div>
     </div>
